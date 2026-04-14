0bd3de7f9971f09fe1b37a3ba70768b371e22788
Tristan273 <lecottytristan2@gmail.com>
2026-04-14 15:40:16 +0200

    First implementation of the physics of the problem

---
+++ physics.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define G 6.674E-11  // attraction force constant
+
+
+/* STRUCTURES */
+struct vector{
+    double x;
+    double y;
+};
+
+struct body{
+    int id;
+
+    double mass;
+    double radius;
+
+    struct vector position;
+
+    struct vector velocity;
+    
+    int type; // if we wish to differentiate stars (type 0), planets (1), asteroids (2),...
+};
+
+
+/* FUNCTIONS */
+
+double norm(struct vector u){
+    return sqrt(u.x*u.x + u.y*u.y);
+}
+
+struct vector add2(struct vector u, struct vector v){
+    struct vector w;
+
+    w.x = u.x + v.x;
+    w.y = u.y + v.y;
+
+    return w;
+}
+
+struct vector addN(struct vector *T, int N){
+    struct vector u;
+    u.x = 0;
+    u.y = 0;
+
+    for(int i = 0; i<N; i++){
+        u = add2(u, T[i]);
+    }
+
+    return u;
+}
+
+struct vector simulate_grav_force2(struct body body1, struct body body2){
+    // The force body1 applies on body2
+    struct vector grav_force;
+    struct vector OM;
+
+    OM.x = body2.position.x - body1.position.x;
+    OM.y = body2.position.y - body1.position.y;
+
+    double d = norm(OM);
+    double d3 = d * d * d;
+    grav_force.x = -G * body1.mass * body2.mass * OM.x / d3;
+    grav_force.y = -G * body1.mass * body2.mass * OM.y / d3;
+    
+    return grav_force;
+}
+
+struct vector simulate_grav_forceN(struct body body1, struct body *T, int N){
+    struct vector grav_force;
+    struct vector grav_force = {0.0, 0.0};
+    
+    for(int i = 0; i<N; i++){
+        grav_force = add2(grav_force, simulate_grav_force2(T[i], body1));
+    }
+
+    return grav_force;
+}
+
+struct body merge_bodies(struct body body1, struct body body2){
+    struct body sum;
+
+    sum.id = body1.id;
+    sum.mass = body1.mass + body2.mass;
+    sum.radius = cbrt(body1.radius*body1.radius*body1.radius + body2.radius*body2.radius*body2.radius);
+
+    sum.position.x = (body1.position.x + body2.position.x)/2;
+    sum.position.y = (body1.position.y + body2.position.y)/2;
+
+    if(body1.mass >= body2.mass){
+        sum.velocity.x = body1.velocity.x;
+        sum.velocity.y = body1.velocity.y;
+    } else {
+        sum.velocity.x = body2.velocity.x;
+        sum.velocity.y = body2.velocity.y;
+    }
+}
+
+void rebound_bodies(struct body *body1, struct body *body2){
+
+    double norm1 = norm(body1->velocity);
+    double norm2 = norm(body2->velocity);
+
+    if (norm1 == 0.0 || norm2 == 0.0) return;
+
+    double x1 = body1->velocity.x;
+    double y1 = body1->velocity.y;
+
+    double x2 = body2->velocity.x;
+    double y2 = body2->velocity.y;
+
+    body1->velocity.x = x2/norm2 * norm1;
+    body1->velocity.y = y2/norm2 * norm1;
+
+    body2->velocity.x = x1/norm1 * norm2;
+    body2->velocity.y = y1/norm1 * norm2;
+}
+
+int main(){
+    return 0;
+}
